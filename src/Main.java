775df8451325e5f3374265cb59bad16d0c6685ce
Non Kositkittiwanit <smartnon2009@hotmail.com>
2017-08-25 12:29:42 +0700

    Move greeting to greet() method.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		greet();
+	}
+
+	public static void greet() {
 		System.out.println("Hello, Git");
 		System.out.println("I am Non Kositkittiwanit.");
 	}
